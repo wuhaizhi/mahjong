2e6ad550de591e3b5989d7d3d7e944a45e2ff1c2
wuhz <lorl_wuhz@outlook.com>
2025-03-20 19:59:57 +0800

    修改洗牌算法

--- internal/biz/shuffle.go
+++ internal/biz/shuffle.go
@@ -3,14 +3,14 @@
 package biz
 
 import (
-	"fmt"
+	"errors"
 	"math/rand"
 	"strconv"
 )
 
 type Shuffle struct {
 	Kinds []MahjongKind
-	Cards []MahjongCard
+	Cards []MahjongTile
 }
 
 func NewShuffle(kinds []MahjongKind) *Shuffle {
@@ -19,73 +19,52 @@ func NewShuffle(kinds []MahjongKind) *Shuffle {
 	}
 }
 
-func (s *Shuffle) Shuffle() ([]MahjongCard, error) {
+func (s *Shuffle) Shuffle() ([]MahjongTile, error) {
 	cards, err := s.GetCards()
 	if err != nil {
-		return nil, err
+		return nil, errors.New("获取麻将牌错误")
 	}
-	cardsLen := len(cards)
+	rand.Shuffle(len(cards), func(i int, j int) {
+		cards[i], cards[j] = cards[j], cards[i]
+	})
 
-	shuffleCards := []MahjongCard{}
-
-	cardsUse := map[int]struct{}{}
-
-	for i := 0; i < cardsLen; i++ {
-		index := rand.Intn(cardsLen - 1)
-
-		fmt.Println("index1:", index)
-		_, ok := cardsUse[index]
-		for ok {
-			if index >= cardsLen-1 {
-				index = 0
-			} else {
-				index++
-			}
-			_, ok = cardsUse[index]
-
-		}
-
-		cardsUse[index] = struct{}{}
-		shuffleCards = append(shuffleCards, cards[index])
-	}
-
-	return shuffleCards, nil
+	return cards, nil
 }
 
-func (s *Shuffle) GetCards() ([]MahjongCard, error) {
-	cards := []MahjongCard{}
+func (s *Shuffle) GetCards() ([]MahjongTile, error) {
+	cards := []MahjongTile{}
 
 	for _, kind := range s.Kinds {
 		switch kind {
 		case Suit:
 			for i := 1; i <= 9; i++ {
 				for j := 0; j < 4; j++ {
-					cards = append(cards, MahjongCard{Type: Dot, Number: i, Name: strconv.Itoa(i) + "筒"})
-					cards = append(cards, MahjongCard{Type: Bamboo, Number: i, Name: strconv.Itoa(i) + "条"})
-					cards = append(cards, MahjongCard{Type: Character, Number: i, Name: strconv.Itoa(i) + "万"})
+					cards = append(cards, MahjongTile{Type: Dot, Number: i, Name: strconv.Itoa(i) + "筒"})
+					cards = append(cards, MahjongTile{Type: Bamboo, Number: i, Name: strconv.Itoa(i) + "条"})
+					cards = append(cards, MahjongTile{Type: Character, Number: i, Name: strconv.Itoa(i) + "万"})
 				}
 			}
 		case Honor:
 			for j := 0; j < 4; j++ {
-				cards = append(cards, MahjongCard{Type: East, Number: 0, Name: "东"})
-				cards = append(cards, MahjongCard{Type: South, Number: 0, Name: "南"})
-				cards = append(cards, MahjongCard{Type: Wast, Number: 0, Name: "西"})
-				cards = append(cards, MahjongCard{Type: North, Number: 0, Name: "北"})
-				cards = append(cards, MahjongCard{Type: Red, Number: 0, Name: "中"})
-				cards = append(cards, MahjongCard{Type: Green, Number: 0, Name: "发"})
-				cards = append(cards, MahjongCard{Type: White, Number: 0, Name: "白"})
+				cards = append(cards, MahjongTile{Type: East, Number: 0, Name: "东"})
+				cards = append(cards, MahjongTile{Type: South, Number: 0, Name: "南"})
+				cards = append(cards, MahjongTile{Type: Wast, Number: 0, Name: "西"})
+				cards = append(cards, MahjongTile{Type: North, Number: 0, Name: "北"})
+				cards = append(cards, MahjongTile{Type: Red, Number: 0, Name: "中"})
+				cards = append(cards, MahjongTile{Type: Green, Number: 0, Name: "发"})
+				cards = append(cards, MahjongTile{Type: White, Number: 0, Name: "白"})
 			}
 
 		case Flower:
 			for j := 0; j < 4; j++ {
-				cards = append(cards, MahjongCard{Type: Spring, Number: 0, Name: "春"})
-				cards = append(cards, MahjongCard{Type: Summer, Number: 0, Name: "夏"})
-				cards = append(cards, MahjongCard{Type: Autumn, Number: 0, Name: "秋"})
-				cards = append(cards, MahjongCard{Type: Winter, Number: 0, Name: "冬"})
-				cards = append(cards, MahjongCard{Type: Plum, Number: 0, Name: "梅"})
-				cards = append(cards, MahjongCard{Type: Orchid, Number: 0, Name: "兰"})
-				cards = append(cards, MahjongCard{Type: Bamboo01, Number: 0, Name: "竹"})
-				cards = append(cards, MahjongCard{Type: Chrysanthemum, Number: 0, Name: "菊"})
+				cards = append(cards, MahjongTile{Type: Spring, Number: 0, Name: "春"})
+				cards = append(cards, MahjongTile{Type: Summer, Number: 0, Name: "夏"})
+				cards = append(cards, MahjongTile{Type: Autumn, Number: 0, Name: "秋"})
+				cards = append(cards, MahjongTile{Type: Winter, Number: 0, Name: "冬"})
+				cards = append(cards, MahjongTile{Type: Plum, Number: 0, Name: "梅"})
+				cards = append(cards, MahjongTile{Type: Orchid, Number: 0, Name: "兰"})
+				cards = append(cards, MahjongTile{Type: Bamboo01, Number: 0, Name: "竹"})
+				cards = append(cards, MahjongTile{Type: Chrysanthemum, Number: 0, Name: "菊"})
 			}
 
 		}
--- internal/biz/shuffle_test.go
+++ internal/biz/shuffle_test.go
@@ -6,17 +6,19 @@ import (
 )
 
 func TestShuffle(t *testing.T) {
-	t.Log("SSSSSSSSSSSSSSSSSSSSSS")
-	// fmt.Println("sssssssdfadfasdfadf")
 	shuffle := NewShuffle([]MahjongKind{Suit, Honor})
 	fmt.Println(shuffle)
-	cards, err := shuffle.Shuffle()
-	if err != nil {
-		t.Error(err)
-	}
-	//
-	fmt.Printf("len: %d\n", len(cards))
-	for _, v := range cards {
-		fmt.Printf("card info : %s", v.Name)
+	for i := 0; i < 100; i++ {
+		cards, err := shuffle.Shuffle()
+		if err != nil {
+			t.Error(err)
+		}
+		//
+		fmt.Printf("len: %d\n", len(cards))
+		for _, v := range cards {
+			fmt.Printf("%s, ", v.Name)
+		}
+		fmt.Println("")
+
 	}
 }
